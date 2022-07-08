d977aebf1204be903b1f58d3bfa9855677285c6d
Mykhailo_Hunko <mykhailo.hunko@gmail.com>
2022-07-08 17:08:29 +0300

    update rabbit_queue_checker lambda

--- lambda.py
+++ lambda.py
@@ -6,15 +6,17 @@ from os import environ
 
 def handler(event=None, context=None):
     try:
-        arbiter = Arbiter(host=environ.get("rabbit_host"), port=5672, user=environ.get("rabbit_project_user"),
-                          password=environ.get("rabbit_project_password"), vhost=environ.get("rabbit_project_vhost"))
+        user = event[0]["user"] if event else environ.get("rabbit_project_user")
+        password = event[0]["password"] if event else environ.get("rabbit_project_password")
+        vhost = event[0]["vhost"] if event else environ.get("rabbit_project_vhost")
+        arbiter = Arbiter(host=environ.get("rabbit_host"), port=5672, user=user, password=password, vhost=vhost)
         try:
             queues = list(arbiter.workers().keys())
         except:
             queues = []
         arbiter.close()
 
-        url = f"{environ.get('galloper_url')}/api/v1/projects/rabbitmq/{environ.get('rabbit_project_vhost')}"
+        url = f"{environ.get('galloper_url')}/api/v1/projects/rabbitmq/{vhost}"
         data = {"queues": json.dumps(queues)}
         headers = {'content-type': 'application/json'}
         if environ.get("token"):
